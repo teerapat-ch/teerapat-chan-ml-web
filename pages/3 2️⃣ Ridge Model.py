bc983cbb0e8daf8b25f66a55945cb906ec278fc5
Teerapat Chanthawatchai <s6604062620018@email.kmutnb.ac.th>
2025-02-21 12:40:26 +0000

    changing input won't remove graph

--- pages/3 2️⃣ Ridge Model.py
+++ pages/3 2️⃣ Ridge Model.py
@@ -7,7 +7,7 @@ import joblib
 model = joblib.load("models/ridge_model.pkl")
 
 @st.cache_data
-def generate_graph():
+def generate_graph(input, prediction, years_list, salary_list):
     plt.figure(figsize=(10,6))
     plt.scatter(years_list, salary_list, color='blue', label="All data Points")
     plt.scatter(input, prediction[0], color='red', label="New data point")
@@ -27,10 +27,18 @@ salary_list = list(salary)
 
 st.header("Ridge Model", divider="grey")
 input = st.number_input("Enter years of experience between 0-40", min_value=0, max_value=40)
+
+if 'prediction' not in st.session_state:
+    st.session_state.prediction = None
+if 'input' not in st.session_state:
+    st.session_state.input = None
+
 submit_button = st.button("Submit")
 
 if submit_button:
-    prediction = model.predict(np.array([[input]]))
-    generate_graph()
-    st.write(f"Years of experience = {input}")
-    st.write(f"Salary = {prediction[0]:.2f}")
+    st.session_state.input = input
+    st.session_state.prediction = model.predict(np.array([[input]]))
+
+if st.session_state.prediction is not None:
+    generate_graph(st.session_state.input, st.session_state.prediction, years_list, salary_list)
+    st.write(f"Salary = {st.session_state.prediction[0]:.2f}")
