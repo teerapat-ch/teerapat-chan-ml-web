485e2854aeba1db5cab3e12a902bd5b339f9c968
Teerapat Chanthawatchai <s6604062620018@email.kmutnb.ac.th>
2025-03-04 09:02:41 +0000

    created pages for deep ff

---
+++ pages/6 🧬 Deep Feed Forward Model.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.header("Deep Feed Forward Model", divider="grey")
+
+submit_button = st.button("Submit")
---
+++ pages/7 🧬 Deep Feed Forward Description.py
@@ -0,0 +1,5 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.header("Deep Feed Forward Description", divider="grey")
