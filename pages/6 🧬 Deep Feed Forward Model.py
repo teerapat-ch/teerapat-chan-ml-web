022a049d7081c1946af1d6151198badaff58e47f
Teerapat Chanthawatchai <s6604062620018@email.kmutnb.ac.th>
2025-03-06 09:57:36 +0000

    please works

--- pages/6 🧬 Deep Feed Forward Model.py
+++ pages/6 🧬 Deep Feed Forward Model.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import numpy as np
-from tensorflow.keras.models import load_model 
+from tensorflow.keras.models import load_model
 
 model = load_model("models/deep_ff_model.keras")
 
